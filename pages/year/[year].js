9c6376ee47a67095e6b942e6f9a2e4ca9015a5e2
Paddy <paddyfedoo@hotmail.com>
2023-07-23 22:43:41 +0100

    Display No Results for years that have no posts

--- pages/year/[year].js
+++ pages/year/[year].js
@@ -63,17 +63,23 @@ export default function Home({ filteredPostData, year, prevYear, nextYear, minYe
             </section>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
-                <ul className={utilStyles.list}>
-                    {filteredPostData.map(({ id, date, title }) => (
-                        <li className={utilStyles.listItem} key={id}>
-                            <Link href={`/posts/${id}`}>{title}</Link>
-                            <br />
-                            <small className={utilStyles.lightText}>
-                                <Date dateString={date} />
-                            </small>
-                        </li>
-                    ))}
-                </ul>
+                {filteredPostData.length > 0 ? (
+                    // If filterdPostsData. length is greater than 0 then display them
+                    <ul className={utilStyles.list}>
+                        {filteredPostData.map(({ id, date, title }) => (
+                            <li className={utilStyles.listItem} key={id}>
+                                <Link href={`/posts/${id}`}>{title}</Link>
+                                <br />
+                                <small className={utilStyles.lightText}>
+                                    <Date dateString={date} />
+                                </small>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    // Otherwise just show a notice that there are No Results
+                    <div className={utilStyles.listItem}>No Results</div>
+                )}
             </section>
 
             <div className={utilStyles.articleNavigation}>
