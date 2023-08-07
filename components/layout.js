90f4a1ffc8c27c5b97efe5ff671626ab9a3515b0
Paddy <paddyfedoo@hotmail.com>
2023-08-07 23:17:21 +0100

    Updated Favicon

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
     <>
       <div className={styles.container}>
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.png" />
           <meta
             name="description"
             content="Blog for Paddy O'Hara"
