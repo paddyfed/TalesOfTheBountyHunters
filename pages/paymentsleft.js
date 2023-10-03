aea6347b7ac5d8eb49c4657dfd74f08ec19e8160
Paddy <paddyfedoo@hotmail.com>
2023-10-03 23:53:25 +0100

    Update page title

--- pages/paymentsleft.js
+++ pages/paymentsleft.js
@@ -2,11 +2,9 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import { PaymentsBuildBlocks } from "../components/paymentsbuildblocks";
 import styles from "../components/paymentsbuildblocks.module.css";
-import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
 
 export default function About() {
-  const title = `${siteTitle} : About`;
+  const title = `${siteTitle} : Payments Left`;
   return (
     <Layout>
       <Head>
