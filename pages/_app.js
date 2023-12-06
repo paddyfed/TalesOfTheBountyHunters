f6b4a3d5347458fa45c7fd0502ced433c7daed5e
Paddy <paddyfedoo@hotmail.com>
2023-12-06 23:31:56 +0000

    Rogue semicolon

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,16 @@
-import '../styles/global.css';
-import Script from 'next/script';
-
+import "../styles/global.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-    return (
-        <>
-            <Component {...pageProps} />;
-            <Script id="stat-counter">
-                {`var sc_project=12880292; 
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script id="stat-counter">
+        {`var sc_project=12880292; 
         var sc_invisible=1; 
-        var sc_security="c0fd9579";` }
-            </Script>
-            <Script src="https://www.statcounter.com/counter/counter.js" />
-        </>
-    );
+        var sc_security="c0fd9579";`}
+      </Script>
+      <Script src="https://www.statcounter.com/counter/counter.js" />
+    </>
+  );
 }
