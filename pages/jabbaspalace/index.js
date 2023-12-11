70927cf673c7d202f59a557766a4858477664b72
Paddy <paddyfedoo@hotmail.com>
2023-12-11 20:24:02 +0000

    Links and upload pics

--- pages/jabbaspalace/index.js
+++ pages/jabbaspalace/index.js
@@ -1,5 +1,8 @@
 import Layout from "./layout";
 import Head from "next/head";
+import Image from "next/image";
+import jabbastyles from "./jabbastyles.module.css";
+import Link from "next/link";
 
 export default function JabbasPalace() {
   const title = "Jabba's Palace";
@@ -9,6 +12,13 @@ export default function JabbasPalace() {
         <title>{title}</title>
       </Head>
       <article>
+        <Image
+          src="https://dummyimage.com/300"
+          width={300}
+          height={300}
+          alt="dummy"
+          className={jabbastyles.left}
+        />
         <p>
           On the far side of the Dune Sea on a small, insignificant planet lives
           the galaxy's most notorious and dangerous gangster. Surrounding
@@ -25,7 +35,13 @@ export default function JabbasPalace() {
           while but before the plot could be executed, Jabba would capture the
           traitors. The usual punishment being a trip to the Rancor pit.
         </p>
-
+        <Image
+          src="https://dummyimage.com/300"
+          width={300}
+          height={300}
+          alt="dummy"
+          className={jabbastyles.right}
+        />
         <p>
           So when a so-called Jedi Knight and his friends entered his palace he
           was surprised and unprepared for how this young man could affect him
@@ -43,42 +59,53 @@ export default function JabbasPalace() {
           and, in my opinion, whether or not the plot could have worked. Some
           related links to the official site are also supplied.
         </p>
+        <Image
+          src="https://dummyimage.com/300"
+          width={300}
+          height={300}
+          alt="dummy"
+          className={jabbastyles.left}
+        />
         <p>
-          The plots are from such characters as
-          <a href="mara.html">Mara Jade</a> the Emperor's Hand,
-          <a href="tessk.html">Tessk</a> the Quarren,
-          <a href="reeyees.html">Ree-Yees</a> the Gran,
-          <a href="dannik.html">Dannik Jerriko</a> the assassin and
-          <a href="bib.html">Bib Fortuna</a> the Twi'lek and they range from
-          killing a Jedi Knight to stealing from the palace, even killing the
-          Hutt himself.
+          The plots are from such characters as&nbsp;
+          <Link href="/jabbaspalace/mara-jade">Mara Jade</Link>&nbsp;the
+          Emperor's Hand,&nbsp;
+          <Link href="/jabbaspalace/tessk">Tessk</Link>&nbsp;the Quarren,&nbsp;
+          <Link href="/jabbaspalace/ree-yees">Ree-Yees</Link>&nbsp;the
+          Gran,&nbsp;
+          <Link href="/jabbaspalace/dannik-jerriko">Dannik Jerriko</Link>
+          &nbsp;the assassin and&nbsp;
+          <Link href="/jabbaspalace/bib-fortuna">Bib Fortuna</Link>&nbsp;the
+          Twi'lek and they range from killing a Jedi Knight to stealing from the
+          palace, even killing the Hutt himself.
         </p>
+
         <p>
           Most of my information came from the books "Tales from Jabba's Palace"
           - Edited by Kevin J. Anderson and "Star Wars: The Essential Guide to
           Characters" by Andy Manguels. The pictures of the conspirators also
           come from this book and were drawn by Michael Butkus. Other pictures
-          are from
-          <a href="http://www.jedinet.com" target="_blank">
+          are from&nbsp;
+          <Link href="http://www.jedinet.com" target="_blank">
             JediNet.com
-          </a>
-          ,
-          <a href="http://www.galacticempire.net" target="_blank">
+          </Link>
+          ,&nbsp;
+          <Link href="http://www.galacticempire.net" target="_blank">
             GalacticEmpire.net
-          </a>
-          and the
-          <a href="http://www.starwars.com" target="_blank">
+          </Link>
+          &nbsp;and the&nbsp;
+          <Link href="http://www.starwars.com" target="_blank">
             Official site
-          </a>
+          </Link>
           .
         </p>
         <p>
           If you are new to "Star Wars" or you haven't read either of these
           books then I have supplied a small index of characters, creatures and
           aliens that are mentioned throughout this site. If you see something
-          that you don't understand, go to the
-          <a href="other.html">Additional Information</a> page and you may find
-          it there.
+          that you don't understand, go to the&nbsp;
+          <Link href="/jabbaspalace/other-info">Additional Information</Link>
+          &nbsp;page and you may find it there.
         </p>
       </article>
     </Layout>
