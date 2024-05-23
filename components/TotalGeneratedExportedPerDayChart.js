7186342eb4b49afe27ea68854159e524a2ec713a
Paddy <paddyfedoo@hotmail.com>
2024-05-23 21:58:20 +0100

    When navigating between solar panel posts, the chart does not update Fixes #44

--- components/TotalGeneratedExportedPerDayChart.js
+++ components/TotalGeneratedExportedPerDayChart.js
@@ -1,6 +1,4 @@
-"use client";
-import { useRef, useEffect, useState } from "react";
-import { Chart } from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
 
 export default function TotalGeneratedExportedPerDayChart({
   month,
@@ -8,66 +6,44 @@ export default function TotalGeneratedExportedPerDayChart({
   exported,
   generated,
 }) {
-  const chartRef = useRef(null);
-  const [exportedData, setExportedData] = useState([]);
-  const [generatedData, setGeneratedData] = useState([]);
-
-  useEffect(() => {
-    setExportedData(exported);
-    setGeneratedData(generated);
-    if (chartRef.current) {
-      if (chartRef.current.chart) {
-        chartRef.current.chart.destroy();
-      }
-
-      const context = chartRef.current.getContext("2d");
-
-      const newChart = new Chart(context, {
-        type: "bar",
-        data: {
-          labels: getDates(parseInt(month), parseInt(year)),
-          datasets: [
-            {
-              data: generatedData,
-              label: "Generated (kWh)",
-            },
-            {
-              data: exportedData,
-              label: "Exported (kWh)",
-            },
-          ],
-        },
-        options: {
-          plugins: {
-            title: {
-              display: true,
-              text: "Total Generated/Exported Per Day",
-            },
-          },
-
-          responsive: true,
-          scales: {
-            x: {
-              stacked: true,
-              grid: {
-                display: false,
-              },
-            },
-            y: {
-              stacked: true,
-            },
-          },
-        },
-      });
-
-      chartRef.current.chart = newChart;
-    }
-  }, [exportedData]);
-
   return (
-    <div>
-      <canvas ref={chartRef} />
-    </div>
+    <Bar
+      data={{
+        labels: getDates(parseInt(month), parseInt(year)),
+        // datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
+        datasets: [
+          {
+            data: generated,
+            label: "Generated (kWh)",
+          },
+          {
+            label: "Exported (kWh)",
+            data: exported,
+          },
+        ],
+      }}
+      options={{
+        plugins: {
+          title: {
+            display: true,
+            text: "Total Generated/Exported Per Day",
+          },
+        },
+
+        responsive: true,
+        scales: {
+          x: {
+            stacked: true,
+            grid: {
+              display: false,
+            },
+          },
+          y: {
+            stacked: true,
+          },
+        },
+      }}
+    />
   );
 }
 
@@ -77,64 +53,3 @@ const getDates = (month, year) =>
       dateStyle: "medium",
     })
   );
-
-{
-  /* <section>
-            <table>
-              <thead>
-                <tr>
-                  <td>Date</td>
-                  <td>Generated</td>
-                  <td>Exported</td>
-                  <td>01/04/2024</td>
-                  <td>02/04/2024</td>
-                  <td>03/04/2024</td>
-                  <td>04/04/2024</td>
-                  <td>05/04/2024</td>
-                  <td>06/04/2024</td>
-                  <td>07/04/2024</td>
-                  <td>08/04/2024</td>
-                  <td>09/04/2024</td>
-                  <td>10/04/2024</td>
-                  <td>11/04/2024</td>
-                  <td>12/04/2024</td>
-                  <td>13/04/2024</td>
-                  <td>14/04/2024</td>
-                  <td>15/04/2024</td>
-                  <td>16/04/2024</td>
-                  <td>17/04/2024</td>
-                  <td>18/04/2024</td>
-                  <td>19/04/2024</td>
-                  <td>20/04/2024</td>
-                  <td>21/04/2024</td>
-                  <td>22/04/2024</td>
-                  <td>23/04/2024</td>
-                  <td>24/04/2024</td>
-                  <td>25/04/2024</td>
-                  <td>26/04/2024</td>
-                  <td>27/04/2024</td>
-                  <td>28/04/2024</td>
-                  <td>29/04/2024</td>
-                  <td>30/04/2024</td>
-                </tr>
-              </thead>
-              <tbody>
-                { {postData.generated.map((item) => {
-                  return (
-                    <tr>
-                      <td>{item}</td>
-                    </tr>
-                  );
-                })}
-
-                {postData.exported.map((item) => {
-                  return (
-                    <tr>
-                      <td>{item}</td>
-                    </tr>
-                  );
-                })} }
-              </tbody>
-            </table>
-          </section> */
-}
