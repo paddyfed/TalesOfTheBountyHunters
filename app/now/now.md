9da20961065b80ddaee865ffcf1340b07b290825
Paddy <paddyfedoo@hotmail.com>
2024-05-28 18:37:57 +0100

    Create a 'now' page Fixes #39

---
+++ app/now/now.md
@@ -0,0 +1,32 @@
+---
+title: "Now"
+date: "2024-05-28"
+---
+
+## Living in
+
+Monaghan
+
+## Working in
+
+Not working at the minute. Currently looking for front-end web development roles and applied for a FIT apprenticeship.
+
+## Reading
+
+### Star Wars
+
+The Princess and the Scoundrel by Beth Revis
+
+### Other
+
+Listening to Foundation by Isaac Asimov on Audible.
+
+Dracula by Bram Stoker on [Dracula by Day](https://dracula-by-day.vercel.app/)
+
+## Podcasts
+
+Dungeons and Daddies. I started listening to this podcast when my son was being born and I've gone back to the start of this again in the last few months. Currently on Season 1 Episode 53 - The Roc.
+
+## Personal projects
+
+The last few personal projects I worked on were this [Star Wars Bookcase](https://star-wars-bookcase.vercel.app/) and the [Dracula by Day](https://dracula-by-day.vercel.app/) site. This is the first year I've been following the Dracula story on a day-by-day basis.
---
+++ app/now/page.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import remarkGfm from "remark-gfm";
+import { unified } from "unified";
+import utilStyles from "../../styles/utils.module.css";
+import Date from "../../components/date";
+
+const postsDirectory = path.join(process.cwd(), "app/now");
+
+export default async function Now() {
+  const postData = await getPostData("now");
+
+  return (
+    <>
+      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+      <div className={utilStyles.lightText}>
+        Last Updated: <Date dateString={postData.date} />
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+    </>
+  );
+}
+
+async function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .process(matterResult.content);
+
+  const contentHtml = processedContent.toString();
+  // Combine the data with the id
+  return { contentHtml, ...matterResult.data };
+}
